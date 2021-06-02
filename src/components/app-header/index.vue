47db100ed510ee6ae5fda7d8793823b17287d453
tangiming <251015172@.com>
2021-06-02 09:36:51 +0800

    feat(components): add basic components

---
+++ src/components/app-header/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="app-header flex-between">
+    <a-space>
+      <img class="logo" src="../../assets/logo.png" alt="logo" />
+      <a class="title" :href="githubLinkUrl">Vue Form Creator</a>
+    </a-space>
+    <div class="app-header-right flex-center">
+      <span class="link-item" @click="handleRedirectGithub">Gitee</span>
+      <GithubOutlined class="github-icon" @click="handleRedirectGithub" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { GithubOutlined } from '@ant-design/icons-vue'
+
+export default {
+  name: 'app-header',
+  components: { GithubOutlined },
+  setup() {
+    const githubLinkUrl = `https://github.com/imRz1015/vue-form-creator`
+    const handleRedirectGithub = () => {
+      window.open(githubLinkUrl)
+    }
+    return { githubLinkUrl, handleRedirectGithub }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../styles/variable';
+
+.app-header {
+  .logo {
+    width: 44px;
+    height: 44px;
+    vertical-align: middle;
+  }
+
+  .title {
+    margin-left: 16px;
+    color: rgba(0, 0, 0, 0.85);
+    font-size: 18px;
+    font-family: Avenir, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji', sans-serif;
+    line-height: 64px;
+    white-space: nowrap;
+    text-decoration: none;
+    font-weight: 500;
+  }
+
+  &-right {
+    font-weight: 500;
+    color: #000000;
+    cursor: pointer;
+
+    .link-item {
+      font-size: 16px;
+      margin-right: 24px;
+      transition: all 0.3s;
+
+      &:hover {
+        color: @themeColor;
+      }
+    }
+
+    .github-icon {
+      transition: all 0.3s;
+      font-size: 28px;
+
+      &:hover {
+        color: @themeColor;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/components-list/componentsConfig.js
@@ -0,0 +1,324 @@
+/* 数据录入 */
+export const basicComponents = [
+  {
+    name: '输入框',
+    type: 'input',
+    icon: 'icon-input',
+    options: {
+      width: '100%',
+      defaultValue: '',
+      required: false,
+      dataType: 'string',
+      pattern: '',
+      placeholder: '',
+      disabled: false
+    }
+  },
+  {
+    name: '文本域',
+    type: 'textarea',
+    icon: 'icon-diy-com-textarea',
+    options: {
+      width: '100%',
+      defaultValue: '',
+      required: false,
+      disabled: false,
+      pattern: '',
+      placeholder: ''
+    }
+  },
+  {
+    name: '数字输入框',
+    type: 'number',
+    icon: 'icon-number',
+    options: {
+      width: '',
+      required: false,
+      defaultValue: 0,
+      min: '',
+      max: '',
+      step: 1,
+      disabled: false,
+      controlsPosition: ''
+    }
+  },
+  {
+    name: '单选框组',
+    type: 'radio',
+    icon: 'icon-radio-active',
+    options: {
+      inline: false,
+      defaultValue: '',
+      showLabel: false,
+      options: [
+        {
+          value: 'Option 1',
+          label: 'Option 1'
+        },
+        {
+          value: 'Option 2',
+          label: 'Option 2'
+        },
+        {
+          value: 'Option 3',
+          label: 'Option 3'
+        }
+      ],
+      required: false,
+      width: '',
+      remote: false,
+      remoteOptions: [],
+      props: {
+        value: 'value',
+        label: 'label'
+      },
+      remoteFunc: '',
+      disabled: false
+    }
+  },
+  {
+    name: '多选框组',
+    type: 'checkbox',
+    icon: 'icon-check-box',
+    options: {
+      inline: false,
+      defaultValue: [],
+      showLabel: false,
+      options: [
+        {
+          value: 'Option 1'
+        },
+        {
+          value: 'Option 2'
+        },
+        {
+          value: 'Option 3'
+        }
+      ],
+      required: false,
+      width: '',
+      remote: false,
+      remoteOptions: [],
+      props: {
+        value: 'value',
+        label: 'label'
+      },
+      remoteFunc: '',
+      disabled: false
+    }
+  },
+  {
+    name: '时间选择框',
+    type: 'time',
+    icon: 'icon-time',
+    options: {
+      defaultValue: '21:19:56',
+      readonly: false,
+      disabled: false,
+      editable: true,
+      clearable: true,
+      placeholder: '',
+      startPlaceholder: '',
+      endPlaceholder: '',
+      isRange: false,
+      arrowControl: true,
+      format: 'HH:mm:ss',
+      required: false,
+      width: ''
+    }
+  },
+  {
+    name: '日期选择框',
+    type: 'date',
+    icon: 'icon-date',
+    options: {
+      defaultValue: '',
+      readonly: false,
+      disabled: false,
+      editable: true,
+      clearable: true,
+      placeholder: '',
+      startPlaceholder: '',
+      endPlaceholder: '',
+      type: 'date',
+      format: 'yyyy-MM-dd',
+      timestamp: false,
+      required: false,
+      width: ''
+    }
+  },
+  {
+    name: '评分',
+    type: 'rate',
+    icon: 'icon-pingfen1',
+    options: {
+      defaultValue: null,
+      max: 5,
+      disabled: false,
+      allowHalf: false,
+      required: false
+    }
+  },
+  {
+    name: '取色器',
+    type: 'color',
+    icon: 'icon-color',
+    options: {
+      defaultValue: '',
+      disabled: false,
+      showAlpha: false,
+      required: false
+    }
+  },
+  {
+    name: '选择器',
+    type: 'select',
+    icon: 'icon-select',
+    options: {
+      defaultValue: '',
+      multiple: false,
+      disabled: false,
+      clearable: false,
+      placeholder: '',
+      required: false,
+      showLabel: false,
+      width: '',
+      options: [
+        {
+          value: 'Option 1'
+        },
+        {
+          value: 'Option 2'
+        },
+        {
+          value: 'Option 3'
+        }
+      ],
+      remote: false,
+      filterable: false,
+      remoteOptions: [],
+      props: {
+        value: 'value',
+        label: 'label'
+      },
+      remoteFunc: ''
+    }
+  },
+  {
+    name: '开关',
+    type: 'switch',
+    icon: 'icon-switch',
+    options: {
+      defaultValue: false,
+      required: false,
+      disabled: false
+    }
+  },
+  {
+    name: '滑动输入条',
+    type: 'slider',
+    icon: 'icon-slider',
+    options: {
+      defaultValue: 0,
+      disabled: false,
+      required: false,
+      min: 0,
+      max: 100,
+      step: 1,
+      showInput: false,
+      range: false,
+      width: ''
+    }
+  },
+  {
+    name: '描述文字',
+    type: 'text',
+    icon: 'icon-wenzishezhi-',
+    options: {
+      defaultValue: 'This is a text',
+      customClass: ''
+    }
+  }
+]
+/* 高级组件 * */
+export const advanceComponents = [
+  {
+    type: 'blank',
+    icon: 'icon-zidingyishuju',
+    options: {
+      defaultType: 'String'
+    }
+  },
+  {
+    type: 'imgupload',
+    icon: 'icon-tupian',
+    options: {
+      defaultValue: [],
+      size: {
+        width: 100,
+        height: 100
+      },
+      width: '',
+      tokenFunc: 'funcGetToken',
+      token: '',
+      domain: 'http://pfp81ptt6.bkt.clouddn.com/',
+      disabled: false,
+      length: 8,
+      multiple: false,
+      isQiniu: false,
+      isDelete: false,
+      min: 0,
+      isEdit: false,
+      action: 'https://jsonplaceholder.typicode.com/photos/'
+    }
+  },
+  {
+    type: 'editor',
+    icon: 'icon-fuwenbenkuang',
+    options: {
+      defaultValue: '',
+      width: ''
+    }
+  },
+  {
+    type: 'cascader',
+    icon: 'icon-jilianxuanze',
+    options: {
+      defaultValue: [],
+      width: '',
+      placeholder: '',
+      disabled: false,
+      clearable: false,
+      remote: true,
+      remoteOptions: [],
+      props: {
+        value: 'value',
+        label: 'label',
+        children: 'children'
+      },
+      remoteFunc: ''
+    }
+  }
+]
+/* 布局组件 */
+export const layoutComponents = [
+  {
+    type: 'grid',
+    icon: 'icon-grid-',
+    columns: [
+      {
+        span: 12,
+        list: []
+      },
+      {
+        span: 12,
+        list: []
+      }
+    ],
+    options: {
+      gutter: 0,
+      justify: 'start',
+      align: 'top'
+    }
+  }
+]
