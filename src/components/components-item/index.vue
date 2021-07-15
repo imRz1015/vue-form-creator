08d73305366549b37ebadf9acf9cd09b5a307d5b
tangiming <251015172@.com>
2021-07-15 17:56:01 +0800

    fix: 未使用函数参数前使用_可以通过eslint

--- src/components/components-item/index.vue
+++ src/components/components-item/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <a-form-item class="components-item" :label="$attrs.name">
-    <component :is="$attrs.type"></component>
+  <a-form-item class="components-item" :label="config.name">
+    <component :is="config.type"></component>
   </a-form-item>
 </template>
 
@@ -9,7 +9,12 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'components-item',
-  setup() {}
+  setup(_props, { attrs }) {
+    const config = attrs
+    return {
+      config
+    }
+  }
 })
 </script>
 
