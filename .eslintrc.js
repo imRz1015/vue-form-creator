74666ac33adcef16c5942cb2b226fb2b96213b21
汤启民 <tangiming@foxmail.com>
2021-08-05 14:55:34 +0800

    fix: 重新配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['plugin:vue/essential', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
+  extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
